ff6af6ce18cc76e87750ea63377918ed467db156
Przemyslaw Bialon <pbialon@sumologic.com>
2023-03-05 11:17:32 +0100

    Created crypto domain model

---
+++ src/domain/crypto/tax.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from domain.crypto.transaction import Transaction
+
+
+class TaxCalculator:
+    def __init__(self):
+        pass
+
+    def calculate(self, transactions: List[Transaction], year: int):
+        pass
---
+++ src/domain/crypto/transaction.py
@@ -0,0 +1,47 @@
+import enum
+
+import pendulum
+
+
+class Action(enum.Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+    def __str__(self):
+        return self.value
+
+
+class FiatValue:
+    def __init__(self, amount: float, currency: str):
+        self.amount = amount
+        self.currency = currency
+
+    def __str__(self):
+        return f"{self.amount} {self.currency}"
+
+
+class CryptoValue:
+    def __init__(self, amount: float, currency: str):
+        self.amount = amount
+        self.currency = currency
+
+    def __str__(self):
+        return f"{self.amount} {self.currency}"
+
+
+class Transaction:
+    def __init__(self,
+                 crypto_value: CryptoValue,
+                 fiat_value: FiatValue,
+                 action: Action,
+                 date: pendulum.DateTime):
+        self.fiat_value = fiat_value
+        self.crypto_value = crypto_value
+        self.action = action
+        self.date = date
+
+    def __str__(self):
+        if self.action == Action.BUY:
+            return f"[{self.date.to_date_string()}]: {self.fiat_value} => {self.crypto_value}"
+        # Action.SELL
+        return f"[{self.date.to_date_string()}]: {self.action} {self.crypto_value} => {self.fiat_value}"
