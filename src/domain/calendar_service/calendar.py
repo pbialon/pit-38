03cfbd787f55ee58f72e171757c9d32ce6d47094
Przemyslaw Bialon <pbialon@sumologic.com>
2023-03-12 11:33:44 +0100

    Calendar with configurable current year, and aware of past years

--- src/domain/calendar_service/calendar.py
+++ src/domain/calendar_service/calendar.py
@@ -1,15 +1,23 @@
+from typing import Iterable
+
 import pendulum
 import holidays
 
-from utils import YEAR
+
+def current_year():
+    return pendulum.now().year
 
 
 class Calendar:
-    def __init__(self):
-        self.holidays = self._get_holidays()
+    YEARS_BACK = 5
+
+    def __init__(self, year: int = current_year()):
+        self.start_year = year - self.YEARS_BACK
+        years = range(self.start_year, year + 1)
+        self.holidays = self._get_holidays(years)
 
     def is_out_of_range(self, day: pendulum.DateTime):
-        return day < pendulum.date(YEAR, 1, 1)
+        return day < pendulum.date(self.start_year, 1, 1)
 
     def is_workday(self, day: pendulum.DateTime):
         return not self._is_weekend(day) and not self._is_holiday(day)
@@ -19,8 +27,9 @@ class Calendar:
 
     def _is_weekend(self, day: pendulum.DateTime):
         # Saturday or Sunday
+        day.weekday()
         return day.format("E") in ("6", "7")
 
-    def _get_holidays(self, year: int = YEAR) -> list:
-        dates = holidays.Poland(years=year).keys()
+    def _get_holidays(self, years: Iterable[int]) -> list:
+        dates = holidays.Poland(years=years).keys()
         return [pendulum.parse(str(date)).date() for date in dates]
