23c1fd2ec4f3334c110ba1e7c67a5735946ce1ad
Przemyslaw Bialon <pbialon@sumologic.com>
2023-03-18 01:07:37 +0100

    Simplified main file

--- src/main.py
+++ src/main.py
@@ -24,29 +24,23 @@ def setup_input_data(filepath: str) -> List[Transaction]:
     return TransactionsCsvReader(filepath, CryptoCsvParser).read()
 
 
-def crypto(tax_year: int, transactions_data_filepath: str, deductable_loss: int):
+def previous_year():
+    return pendulum.now().year - 1
+
+@click.command()
+@click.option('--tax-year', '-y', default=previous_year(), help='Year you want to calculate tax for')
+@click.option('--filepath', '-f', help='Path to csv file with transactions')
+@click.option('--deductable-loss', '-l', default=-1,
+              help='Deductable loss from previous years. It overrides calculation of loss by the script',
+              type=float)
+def crypto(tax_year: int, filepath: str, deductable_loss: int):
     tax_year_start = pendulum.date(tax_year, 1, 1)
     tax_year_end = pendulum.date(tax_year, 12, 31)
     tax_calculator = setup_tax_calculator(tax_year_start, tax_year_end)
-    transactions = setup_input_data(transactions_data_filepath)
+    transactions = setup_input_data(filepath)
     tax_data = tax_calculator.calculate_tax_per_year(transactions, tax_year, deductable_loss)
     print(tax_data, end='\n\n')
 
 
-def previous_year():
-    return pendulum.now().year - 1
-
-
-@click.command()
-@click.option('--filepath', '-f', help='Path to csv file with transactions')
-@click.option('--tax-year', '-y', default=previous_year(), help='Year you want to calculate tax for')
-@click.option('--deductable-loss', '-l', default=-1,
-              help='Deductable loss from previous years. It overrides calculation of loss by the script',
-              type=float)
-def main(filepath, tax_year, deductable_loss):
-    # TODO: add other options than just revolut file
-    crypto(tax_year, filepath, deductable_loss)
-
-
 if __name__ == "__main__":
-    main()
+    crypto()
