153e1124507e4f82f149c4cb8eafdcc2deac9411
Przemyslaw Bialon <pbialon@sumologic.com>
2023-03-27 20:21:42 +0200

    cleanup exchanger tests

--- tests/test_exchanger.py
+++ tests/test_exchanger.py
@@ -7,6 +7,11 @@ from domain.currency_exchange_service.currencies import FiatValue, Currency
 from domain.currency_exchange_service.exchanger import Exchanger
 
 
+class ExchangeRateProviderStub:
+    def get_rate(self, currency, date):
+        return 4.0
+
+
 class TestExchanger(TestCase):
     def test_get_day_one(self):
         exchanger = Exchanger(None, Calendar())
@@ -24,23 +29,13 @@ class TestExchanger(TestCase):
         self.assertEqual(day_one, pendulum.date(2022, 1, 10))
 
     def test_exchange(self):
-        class ExchangeRateProviderStub:
-            def get_rate(self, currency, date):
-                return 4.0
-
         exchanger = Exchanger(ExchangeRateProviderStub(), Calendar())
         amount = FiatValue(100, Currency.DOLLAR)
         amount_in_pln = exchanger.exchange(pendulum.date(2022, 1, 4), amount)
-        self.assertEqual(amount_in_pln.currency, Currency.ZLOTY)
-        self.assertEqual(amount_in_pln.amount, 400)
+        self.assertEqual(amount_in_pln, FiatValue(400, Currency.ZLOTY))
 
     def test_exchange_the_same_currency(self):
-        class ExchangeRateProviderStub:
-            def get_rate(self, currency, date):
-                return 4.0
-
         exchanger = Exchanger(ExchangeRateProviderStub(), Calendar())
         amount = FiatValue(100, Currency.ZLOTY)
         amount_in_pln = exchanger.exchange(pendulum.date(2022, 1, 4), amount)
-        self.assertEqual(amount_in_pln.currency, Currency.ZLOTY)
-        self.assertEqual(amount_in_pln.amount, 100)
+        self.assertEqual(amount_in_pln, FiatValue(100, Currency.ZLOTY))
