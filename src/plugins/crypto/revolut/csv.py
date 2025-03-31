7953242d4fd57e8e8ea43d425819ca2b51de5250
matbur <mateusz@burniak.pl>
2025-03-31 16:54:47 +0200

    chore: remove unused method

--- src/plugins/crypto/revolut/csv.py
+++ src/plugins/crypto/revolut/csv.py
@@ -23,18 +23,3 @@ class CsvService:
 
         logger.info(f"Parsed {len(transactions)} transactions")
         return transactions
-
-    def save(self, transactions: List[Transaction], output_path: str):
-        with open(output_path, 'w') as csvfile:
-            # todo: move it to domain or data_sources
-            writer = csv.DictWriter(csvfile, fieldnames=["date", "operation", "amount", "symbol", "fiat_value", "currency"])
-            writer.writeheader()
-            for transaction in transactions:
-                writer.writerow({
-                    "date": transaction.date.to_datetime_string(),
-                    "operation": transaction.action,
-                    "amount": f"{float(transaction.asset.amount):.8f}",
-                    "symbol": transaction.asset.asset_name,
-                    "fiat_value": f"{float(transaction.fiat_value.amount):.2f}",
-                    "currency": transaction.fiat_value.currency
-                })
